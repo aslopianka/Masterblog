83b71255f04f0b45b7af316fe339597563d635fe
aslopianka <aslopianka12@gmail.com>
2026-06-16 16:48:55 +0200

    implemented adding a post functionality with style updates for html templates

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
-from utils import load_data_from_json_file
+from utils import load_data_from_json_file, write_data_to_json_file
 
 app = Flask(__name__)
 
@@ -9,6 +9,24 @@ app = Flask(__name__)
 def index():
     blog_posts = load_data_from_json_file('storage.json')
     return render_template('index.html', posts=blog_posts)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        all_posts = load_data_from_json_file('storage.json')
+
+        new_post = {
+            'id': len(all_posts) + 1,
+            'title': request.form.get('title'),
+            'author': request.form.get('author'),
+            'content': request.form.get('content')
+        }
+        all_posts.append(new_post)
+        write_data_to_json_file('storage.json', all_posts)
+
+        return redirect(url_for('index'))
+
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -4,3 +4,8 @@ import json
 def load_data_from_json_file(filename):
     with open(filename, 'r') as f:
         return json.load(f)
+
+
+def write_data_to_json_file(filename, data):
+    with open(filename, 'w') as f:
+        json.dump(data, f)
