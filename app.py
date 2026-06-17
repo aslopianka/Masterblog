01356d76b52ad7c6615960c8959137332f438bfd
aslopianka <aslopianka12@gmail.com>
2026-06-17 10:58:30 +0200

    changed port back to 5000 for codio compliance

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def update(post_id):
         return "Post not found", 404
 
     if request.method == 'POST':
-    
+
         updated_post = {
             'id': post_id,
             'title': request.form.get('title'),
@@ -65,4 +65,4 @@ def update(post_id):
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
